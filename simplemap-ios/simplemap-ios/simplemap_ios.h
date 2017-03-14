f0f87f3e7989d6dfb2b0729cc2e7ef819e67eea6
Nina <nina.manalo19@gmail.com>
2017-03-14 20:52:06 +0400

    Initial commit iOS

---
+++ simplemap-ios/simplemap-ios/simplemap_ios.h
@@ -0,0 +1,13 @@
+//
+//  simplemap_ios.h
+//  simplemap-ios
+//
+//  Created by Nina Manalo on 14/03/2017.
+//  Copyright © 2017 Nina Manalo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface simplemap_ios : NSObject
+
+@end
